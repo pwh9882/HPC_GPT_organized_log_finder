bb03d8257b74e5c1b180bf246ec620e84252f4e0
handsomemin <handsomemin@kookmin.ac.kr>
2024-07-23 15:03:12 +0900

    파편적 주석 제거 및 코드 정리하였음. develpment에 merge 하는 version(팀장 검토 완료)

--- embedding/EmbeddingSummary.py
+++ embedding/EmbeddingSummary.py
@@ -34,7 +34,6 @@ class EmbeddingSummary:
         )
         
         self.index_path = "인덱스 file_경로" # 인덱스 파일 경로
-        self.vectorstore = None  # 초기에는 None으로 설정
         
         self.load_or_create_vector_db()  # init 단계에서 load or create vector db()에 대한 것 반영. 초기화 시 벡터스토어를 로드하거나 생성
         
@@ -43,7 +42,6 @@ class EmbeddingSummary:
     def embed_and_store_summary(self, summary: str, user_id: str, conversation_id: str):
         metadata = {"user_id": user_id, "conversation_id": conversation_id}
     
-        # self.load_or_create_vector_db(user_id, conversation_id) # 수정해야함 - 해당 코드를 임베드하고 저장할 때 마다 호출하지 않고, init 단계에서 먼저 불러와놓고, 사용하기
         self.vectorstore.add_texts([summary], embeddings=self.embeddings, metadatas=[metadata]) # 
         self.vectorstore.save_local(self.index_path) # 업데이트된 벡터스토어를 저장
 
@@ -57,17 +55,5 @@ class EmbeddingSummary:
         else:
             self.vectorstore = FAISS(self.embeddings) # 없음 -> 새로운 vec store 추가
             
-            # <<이쪽에서 save_local에 대한 logic 추가>>
+            # 이쪽에서도 save_local 해야 함
             self.vectorstore.save_local(self.index_path)
-            
-        # return self.vectorstore : 할 필요 없음. 왜냐면 함수를 call하는 것 자체만으로 설정이 되기에
-    
-        # 사용자 ID와 대화 ID에 해당하는 엔트리가 벡터 스토어에 존재하는지 chk하는 logic - 필요없음
-        # existing_entries = self.vectorstore.search({"user_id": user_id, "conversation_id": conversation_id}) # 기존 DB 로드
-        # existing_entries = self.vectorstore.search({"user_id": user_id, "conversation_id": conversation_id}) # 기존 DB 로드
-        # if not existing_entries:
-        #     self.vectorstore = FAISS(self.embeddings)  # 새로운 DB 생성
-        
-        # return self.vectorstore
-    
-    
